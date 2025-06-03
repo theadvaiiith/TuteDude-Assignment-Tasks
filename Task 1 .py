2b8a5eacf51c95d0415216188d1d749426f3ab56
theadvaiiith <161201281+theadvaiiith@users.noreply.github.com>
2025-06-03 13:42:32 +0530

    Add files via upload

---
+++ Task 1 .py
@@ -0,0 +1,11 @@
+p = float(input("Enter a number : "))
+
+q = float(input("Enter another number : "))
+
+print("Addition: ", p + q)
+
+print("Subtraction: ", p - q)
+
+print("Multiplication: ", p * q)
+
+print("Division: ", p / q)
---
+++ Task 2 .py
@@ -0,0 +1,2 @@
+first_name, last_name  = input("Enter your first name: ").strip(), input("Enter your last name: ").strip()
+print("Hello "+first_name+" "+last_name+", welcome to the Python programme")
